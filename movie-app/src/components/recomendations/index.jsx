deb9b50d627ba15f87ef9b3bb99c6d351a991d15
miceflicts <nickzinhomice@gmail.com>
2023-03-18 21:06:42 -0300

    adding tmdb api

--- movie-app/src/components/recomendations/index.jsx
+++ movie-app/src/components/recomendations/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import CardCarousel from '../cardCarousel'
 
 function Recomended({title}) {
+  // get the movies here
 
   return (
     <>
-    <div className='flex flex-col justify-start mb-10'>
+    <div className='flex flex-col justify-start mb-5'>
         <h5 className='text-white font-medium text-lg ml-2'>{title}</h5>
     </div>
-    <div className='mb-10'>
+    <div className='mb-14'>
       <CardCarousel></CardCarousel>
     </div>
     </>
