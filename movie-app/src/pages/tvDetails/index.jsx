9d7b61c3be759ff0fc20b1c0d511271e23c81ae7
miceflicts <nickzinhomice@gmail.com>
2023-03-23 22:47:30 -0300

    added person page

--- movie-app/src/pages/tvDetails/index.jsx
+++ movie-app/src/pages/tvDetails/index.jsx
@@ -3,10 +3,11 @@ import { useParams } from 'react-router-dom';
 
 function TvShowDetails() {
     const {id} = useParams();
+    const language =  navigator.language || navigator.userLanguage; 
   
     return (
       <>
-         <div>Tv Details for id: {id}</div>;
+         <div className='m-auto w-3/4 mt-20 max-[600px]:w-[95%] min-h-[800px]'>Tv Details for id: {id}</div>;
       </>
     )
   }
