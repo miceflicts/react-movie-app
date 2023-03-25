fb5433a8c3cfe2d3c5c27a01e328773ad9e22dca
miceflicts <nickzinhomice@gmail.com>
2023-03-24 21:43:38 -0300

    fixed bugs

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -6,7 +6,7 @@ function SearchCards({name, poster, type, id}) {
   return (
     <Link to={`/${type}/${id}`} className='contents'>
         <div className="flex flex-col max-[900px]:min-w-[100px]" id='cardDiv'>
-            <img src={poster} className="h-full transition delay-75 hover:brightness-50 aspect-[4/6] rounded-md" />
+            <img src={poster} className="transition delay-75 hover:brightness-50 aspect-[4/6] rounded-md" />
             <h5 className='text-gray-200/90 break-all mt-5'>{name}</h5>
         </div>
     </Link>
