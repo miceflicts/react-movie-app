848335d266fe56253b52928928857a6acbfd98a6
miceflicts <nickzinhomice@gmail.com>
2023-03-26 02:10:03 -0300

    updated the medias page

--- src/pages/movieDetails/index.jsx
+++ src/pages/movieDetails/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import MainInfos from '../../components/mediaDetails/mainInfos';
+import Footer from '../../layouts/footer';
 import { useParams } from 'react-router-dom';
 
 function MovieDetails() {
     const {id} = useParams();
+    const type = "movie";
     const language =  navigator.language || navigator.userLanguage; 
   
       return (
         <>
-           <div className='m-auto w-3/4 mt-20 max-[600px]:w-[95%] min-h-[800px]'>Movie Details for id: {id}</div>;
-        </>
+          <div className='m-auto w-3/4 mt-20 max-[1300px]:w-[95%] min-h-[800px] gap-10 flex flex-col'>
+              <div className='flex gap-10'>
+                <MainInfos type={type} id={id} language={language}></MainInfos>
+              </div>
+              <Footer language={language}></Footer>
+          </div>
+      </>
       )
     }
 
