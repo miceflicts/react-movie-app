a48622e134f700d34f4fa3fd3a260ada4d0d9a31
miceflicts <nickzinhomice@gmail.com>
2023-03-26 21:37:48 -0300

    Added Cast component

--- src/pages/tvDetails/index.jsx
+++ src/pages/tvDetails/index.jsx
@@ -2,18 +2,27 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import Footer from '../../layouts/footer';
 import MainInfos from '../../components/mediaDetails/mainInfos';
+import Cast from '../../components/mediaDetails/cast';
 
 function TvShowDetails() {
+    const [mediaInfo, setMediaInfo] = useState(null)
     const {id} = useParams();
     const type = "tv";
     const language =  navigator.language || navigator.userLanguage; 
+
+    const handleFetch = (event) => {
+      setMediaInfo(event);
+    }
+    
   
     return (
       <>
-          <div className='m-auto w-3/4 mt-20 max-[1300px]:w-[95%] min-h-[800px] gap-10 flex flex-col'>
+          <div className='m-auto w-3/4 mt-20 max-[1300px]:w-[95%] min-h-[800px] gap-10'>
               <div className='flex gap-10'>
-                <MainInfos type={type} id={id} language={language}></MainInfos>
+                <MainInfos type={type} id={id} language={language} onFetch={handleFetch}></MainInfos>
               </div>
+              
+              <Cast info={mediaInfo}></Cast>
               <Footer language={language}></Footer>
           </div>
       </>
