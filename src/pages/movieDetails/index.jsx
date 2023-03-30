754e031ab64b62e0784a58254d6b8e987b4d50ca
miceflicts <nickzinhomice@gmail.com>
2023-03-29 21:55:50 -0300

    Added scroll to in media

--- src/pages/movieDetails/index.jsx
+++ src/pages/movieDetails/index.jsx
@@ -1,9 +1,9 @@
-import React,{useState} from 'react'
+import React,{useState, useRef} from 'react'
+import { useParams } from 'react-router-dom';
 import MediaMainInfos from '../../components/mediaDetails/mainInfos';
 import MediaCast from '../../components/mediaDetails/cast';
 import MediaBackdrops from '../../components/mediaDetails/backdrops';
 import Footer from '../../layouts/footer';
-import { useParams } from 'react-router-dom';
 import MediaPosters from '../../components/mediaDetails/posters';
 import MediaRecommendations from '../../components/mediaDetails/recomendations';
 import MediaVideos from '../../components/mediaDetails/videos';
@@ -14,20 +14,29 @@ function MovieDetails() {
   const type = "movie";
   const language =  navigator.language || navigator.userLanguage; 
 
+  const videoRef = useRef(null);
+
   const handleFetch = (event) => {
     setMediaInfo(event);
+  }
+
+  const handleExecuteScroll = () => {
+    videoRef.current.scrollIntoView({ behavior: 'smooth' });
   }
   
   return (
     <>
         <div className='m-auto w-3/4 mt-20 max-[1300px]:w-[95%] min-h-[800px] gap-10'>
             <div className='flex gap-10'>
-              <MediaMainInfos type={type} id={id} language={language} onFetch={handleFetch}></MediaMainInfos>
+              <MediaMainInfos type={type} id={id} language={language} onFetch={handleFetch} onExecuteScroll={handleExecuteScroll}></MediaMainInfos>
             </div>
             
             <MediaCast info={mediaInfo}></MediaCast>
             <MediaBackdrops info={mediaInfo}></MediaBackdrops>
-            <MediaVideos info={mediaInfo}></MediaVideos>
+            <div ref={videoRef}>
+              <MediaVideos info={mediaInfo}></MediaVideos>
+            </div>
+            
             <MediaPosters info={mediaInfo}></MediaPosters>
             <MediaRecommendations info={mediaInfo}></MediaRecommendations>
 
